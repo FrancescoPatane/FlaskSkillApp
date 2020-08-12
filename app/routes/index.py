451ca9c7e3c4f352fd840088a7c0cc8ba250352d
fpatane <fpatane@pccube.com>
2020-08-12 21:37:41 +0200

    fe

--- app/routes/index.py
+++ app/routes/index.py
@@ -8,6 +8,12 @@ routes = Blueprint('navigation_routes', __name__)
 
 
 @routes.route("/")
-def main():
+def index_route():
     renderPath = Path(__file__).parent.parent / "templates/index.html"
+    return render_template(renderPath.name)
+
+
+@routes.route("/ledger/employees")
+def employees_ledger():
+    renderPath = Path(__file__).parent.parent / "templates/employees.html"
     return render_template(renderPath.name)
---
+++ app/static/js/employees.js
@@ -0,0 +1,37 @@
+
+function fetchEmployeesData() {
+  fetch('/employees')
+    .then(response => response.json()
+      .then(result => setEmplTableHtml(result)))
+    .then(data => console.log(data))
+}
+
+function setEmplTableHtml(employees) {
+  let tableHtml = ""
+  employees.map(e => tableHtml += "<tr><td><button type='button' class='btn btn-light' onclick=\"selectEmployee('" + e.email + "')\"><i class='fa fa-search fa-2' ></i></button></td><td>" + e.name + "</td><td>" + e.surname + "</td><td>" + e.email + "</td><td>" + e.role + "</td></tr>")
+  document.getElementById('employee-table-body').innerHTML = tableHtml
+  $('#employee-table').DataTable()
+}
+
+function selectEmployee(email){
+  fetch('/employees/'+email)
+    .then(
+      response => response.json()
+      .then(result => populateEmployeeCard(result))
+    )
+    .then(
+      data => console.log(data)
+    )
+}
+
+function populateEmployeeCard(employee){
+  document.getElementById('empl-name').innerHTML = employee.name + " " + employee.surname
+  document.getElementById('empl-data').innerHTML = employee.email + " - " + employee.role
+  let skillsHtml = ""
+  employee.skills.map(e => skillsHtml += "<li class='list-group-item'>" + e.skill_name + "</li>")
+  document.getElementById('empl-skills').innerHTML = skillsHtml
+}
+
+
+
+employees = fetchEmployeesData()
