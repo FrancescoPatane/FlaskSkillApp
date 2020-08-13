a4595fb6173bd32baf6d0ec262a3d1b5c2623367
fpatane <fpatane@pccube.com>
2020-08-13 17:51:53 +0200

    db heroku

--- config.py
+++ config.py
@@ -1,10 +1,2 @@
-
-
-# SQLALCHEMY_DATABASE_URI ='mysql://root:password@localhost/py'
-# SQLALCHEMY_DATABASE_URI="oracle://py_skill:py_skill@//localhost/xe"
-SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:postgres@localhost/pyskill'
+SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://aejyarxlpqjnxb:a67e6c03ccea2a1d07e7098cd95391ba3b9cd2de1ac48643e729ab919b4c1993@ec2-184-73-249-9.compute-1.amazonaws.com:5432/de5vs725ci616g'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
-DB_HOST='localhost'
-DB_USER='root'
-DB_PASSWORD='password'
-DB_DATABASE='py'
