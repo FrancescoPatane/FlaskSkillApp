d0bec2585c1737c137e0fe6041404018a17f9d20
fpatane <fpatane@pccube.com>
2020-09-06 19:51:11 +0200

    fix

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 
 
-SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://aejyarxlpqjnxb:a67e6c03ccea2a1d07e7098cd95391ba3b9cd2de1ac48643e729ab919b4c1993@ec2-184-73-249-9.compute-1.amazonaws.com:5432/de5vs725ci616g'
-# SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:postgres@localhost/pyskill'
+SQLALCHEMY_DATABASE_URI=''
 SQLALCHEMY_TRACK_MODIFICATIONS=False
